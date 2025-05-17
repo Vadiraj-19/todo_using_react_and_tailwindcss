863919a6c7fbb27d7c8e3a8c56eb4ad81fdb6520
Vadiraj-19 <Vadirajbetageri1234@gmail.com>
2025-05-17 16:59:38 +0530

    todo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full h-10 bg-black text-white flex justify-center items-center"><i className="fa-solid fa-copyright"></i> 2025 Vadiraj Betageri</div>
+    <div className="fixed bottom-0 w-full h-10 bg-black text-white flex justify-center items-center"><i className="fa-solid fa-copyright"></i> 2025 Vadiraj Betageri   <a href="https://github.com/Vadiraj-19/todo_using_react_and_tailwindcss"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYiBwe7URF5iyjMW20qp4IvMNvys8-tGb_-A&s" className=" w-8 h-8 object-cover object-center rounded-full ml-5" /></a></div>
   )
 }
 
